c5dc9a9a8c0197e54eec8704ce8ef702e30ba934
Nahalil <nahalilmuchtar2@gmail.com>
2025-11-08 23:55:56 +0800

    add check endpoint

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,5 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import { LoginService, LogOutService, RegisterService } from "../services/authServices";
+import {
+  LoginService,
+  LogOutService,
+  RegisterService,
+} from "../services/authServices";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { JWT_ACCESS_SECRET } from "../config";
+import { prisma } from "../lib/prisma";
+import { AppError } from "../utils/appError";
 
 export async function LoginController(
   req: Request,
@@ -9,9 +17,7 @@ export async function LoginController(
   try {
     const accessToken = await LoginService(req.body);
 
-    res
-      .status(200)
-      .json({ message: `Login successfully`, accessToken});
+    res.status(200).json({ message: `Login successfully`, accessToken });
   } catch (error) {
     next(error);
   }
@@ -27,9 +33,7 @@ export async function LogOutController(
 
     if (accessToken) await LogOutService(accessToken);
 
-    res
-      .status(200)
-      .json({ message: `Log out successfully` });
+    res.status(200).json({ message: `Log out successfully` });
   } catch (error) {
     next(error);
   }
@@ -45,6 +49,38 @@ export async function RegisterController(
 
     res.status(201).json({ message: `New user created successfully`, newUser });
   } catch (error) {
+    next(error);
+  }
+}
+
+export async function authController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.body.accessToken as string;
+
+  try {
+    if (token) {
+      verify(token, String(JWT_ACCESS_SECRET)) as JwtPayload;
+
+      const tokenInDb = await prisma.tokens.findUnique({
+        where: {
+          token
+        }
+      })
+
+      if (tokenInDb?.isValid === true) {
+        res.status(200).json({ message: `User valid`, isLoggedIn: true });
+      } else {
+        throw new AppError("Token invalid", 401);
+      }
+
+    } else {
+      res.status(200).json({ message: `Not Logged In`, isLoggedIn: false });
+    }
+  } catch (error) {
+    await LogOutService(token);
     next(error);
   }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { LoginSchema, RegisterSchema } from "../schemas/authSchemas";
 import { validateRequest } from "../middlewares/validationMiddleware";
-import { LoginController, LogOutController, RegisterController } from "../controllers/authControllers";
+import { authController, LoginController, LogOutController, RegisterController } from "../controllers/authControllers";
 
 const router = Router();
 
 router.post("/login", validateRequest(LoginSchema), LoginController);
 router.post("/register", validateRequest(RegisterSchema), RegisterController);
 router.post("/logout", LogOutController);
+router.post("/check", authController);
 
 export default router;
